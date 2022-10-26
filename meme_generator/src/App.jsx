4dedcfe63f5b7829a6c3a8a7f700853a1a9ce2e6
PedroMarMol <martosmoleropedro@gmail.com>
2022-10-26 08:50:24 +0200

    create and style meme form section

--- meme_generator/src/App.jsx
+++ meme_generator/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Meme from './components/Meme'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Meme />
     </div>
   )
 }
---
+++ meme_generator/src/components/Meme.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <div className="form--inputs">
+                    <input></input>
+                    <input></input>
+                </div>
+                <button className="form--button">Get a new meme image 🖼</button>
+            </form>
+        </main>
+    )
+}
