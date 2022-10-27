e99ebb92a58edde9e14b72f94793547d46b6f9d5
PedroMarMol <martosmoleropedro@gmail.com>
2022-10-27 11:13:42 +0200

    refactor state

--- meme_generator/src/components/Meme.jsx
+++ meme_generator/src/components/Meme.jsx
@@ -2,12 +2,39 @@ import React from "react";
 import memesData from "../memesData";
 
 export default function Meme() {
-    const [memeImage, setMemeImage] = React.useState("");
+    /**
+     * Challenge: Update our state to save the meme-related
+     * data as an object called `meme`. It should have the
+     * following 3 properties:
+     * topText, bottomText, randomImage.
+     * 
+     * The 2 text states can default to empty strings for now,
+     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+     * 
+     * Next, create a new state variable called `allMemeImages`
+     * which will default to `memesData`, which we imported above
+     * 
+     * Lastly, update the `getMemeImage` function and the markup 
+     * to reflect our newly reformed state object and array in the
+     * correct way.
+     */
+
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    
     function getMemeImage() {
         const memesArray = memesData.data.memes
-        const randomMeme = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomMeme].url)
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme( prevState => ({
+            ...prevState,
+            randomImage: url
+        }))
     }
+    
     return (
         <main>
             <div className="form">
@@ -16,7 +43,7 @@ export default function Meme() {
                     <input></input>
                 </div>
                 <button onClick={getMemeImage} className="form--button">Get a new meme image 🖼</button>
-                <img className="form--image" src={memeImage}/>
+                <img className="form--image" src={meme.randomImage}/>
             </div>
         </main>
     )
